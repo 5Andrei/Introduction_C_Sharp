08fa51a79e8abbb92bf121a02cc2649556d2bbcb
5Andrei <120333207+5Andrei@users.noreply.github.com>
2023-05-27 18:34:07 +0300

    add Howe work file

---
+++ Seminar_1/Home_Work_1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Console.Write(" Enter First Digit: ");
+double firstDigit = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(" Enter Second Digit: ");
+double secondDigit = Convert.ToInt32(Console.ReadLine());
+
+double maxDigit = secondDigit;
+if (firstDigit > maxDigit) maxDigit = firstDigit;
+Console.Write(" Max digit is: " + maxDigit);
+*/
+/*
+Console.Write(" Enter First Digit: ");
+double firstDigit = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Enter Second Digit: ");
+double secondDigit = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Enter Third Digit: ");
+double thirdDigit = Convert.ToInt32(Console.ReadLine());
+
+double maxDigit = firstDigit;
+if (firstDigit<secondDigit) maxDigit= secondDigit;
+if (thirdDigit>secondDigit) maxDigit= thirdDigit;
+
+Console.Write(" Max digit is: " + maxDigit);
+*/
--- Seminar_1/Square_example/Program.cs
+++ Seminar_1/Square_example/Program.cs
@@ -1,11 +1,11 @@
-﻿// Console.Write("Enter digit: ");
+﻿//  Console.Write("Enter digit: ");
 
-// int digit = Convert.ToInt32(Console.ReadLine());
-// int square = digit * digit;
+//  int digit = Convert.ToInt32(Console.ReadLine());
+//  int square = digit * digit;
 
-// double squareSecOpt = Math.Pow(digit, 2);
-// Console.WriteLine("Число " + digit + " В квадрате " + squareSecOpt);
-
+//  double squareSecOpt = Math.Pow(digit, 2);
+//  Console.WriteLine("Число " + digit + " В квадрате " + squareSecOpt);
+/*
 Console.Write("Enter First digit: ");
 int firstDigit = Convert.ToInt32(Console.ReadLine());
 
@@ -14,5 +14,20 @@ int secondDigit = Convert.ToInt32(Console.ReadLine());
 
 if (firstDigit == secondDigit * secondDigit)
 {
-Console.WriteLine("Число " + firstDigit + " Является  квадратом числа " + secondDigit);
+    Console.WriteLine("Число " + firstDigit + " Является  квадратом числа " + secondDigit);
+}
+else
+{
+    Console.WriteLine("Число " + firstDigit + " Не является  квадратом числа " + secondDigit);
+}
+*/
+Console.Write("Enter N digit: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int negative_N = N * (-1);
+
+while (negative_N <= N)
+{
+    Console.WriteLine(negative_N);
+
+    negative_N++;
 }
