c69a80b601c168de586fb313ff14003ba2f29c52
Andrei <120333207+5Andrei@users.noreply.github.com>
2023-05-28 11:18:50 +0300

    Update Home work file

--- Seminar_1/Home_Work_1/Program.cs
+++ Seminar_1/Home_Work_1/Program.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Console.Write(" Enter First Digit: ");
 double firstDigit = Convert.ToInt32(Console.ReadLine());
 
@@ -8,8 +8,8 @@ double secondDigit = Convert.ToInt32(Console.ReadLine());
 double maxDigit = secondDigit;
 if (firstDigit > maxDigit) maxDigit = firstDigit;
 Console.Write(" Max digit is: " + maxDigit);
-*/
-/*
+
+
 Console.Write(" Enter First Digit: ");
 double firstDigit = Convert.ToInt32(Console.ReadLine());
 Console.Write(" Enter Second Digit: ");
@@ -18,8 +18,30 @@ Console.Write(" Enter Third Digit: ");
 double thirdDigit = Convert.ToInt32(Console.ReadLine());
 
 double maxDigit = firstDigit;
-if (firstDigit<secondDigit) maxDigit= secondDigit;
-if (thirdDigit>secondDigit) maxDigit= thirdDigit;
-
+if (firstDigit<secondDigit) 
+{
+    maxDigit= secondDigit;
+}
+if (thirdDigit>secondDigit) 
+{
+    maxDigit= thirdDigit;
+}
 Console.Write(" Max digit is: " + maxDigit);
+
+
+Console.Write(" Enter Digit: ");
+double digit = Convert.ToInt32(Console.ReadLine());
+double tempDigit = digit % 2;
+
+if (tempDigit == 0) Console.Write(" Yes ");
+    else Console.Write(" No ");
+
+Console.Write(" Enter Digit_N: ");
+int Digit_N = Convert.ToInt32(Console.ReadLine());
+int temp_Digit = Digit_N - Digit_N + 2;
+while (temp_Digit <= Digit_N)
+{
+    Console.WriteLine(temp_Digit);
+    temp_Digit = temp_Digit + 2;
+}
 */
