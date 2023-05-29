e9abc5fbef37c895b3b1f20a3c3e4b1a018f0e11
5Andrei <120333207+5Andrei@users.noreply.github.com>
2023-05-29 13:41:54 +0300

    new Example009IntroMethod

--- Example009IntroMethod/Program.cs
+++ Example009IntroMethod/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 23;
+int b1 = 66;
+int c1 = 33;
+int a2 = 23;
+int b2 = 66;
+int c2 = 33;
+int a3 = 23;
+int b3 = 66;
+int c3 = 78;
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2),Max(a3, b3, c3));
+Console.WriteLine(max);
