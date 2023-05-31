73acb17a378111ad21780ab20da88088eec117ec
5Andrei <120333207+5Andrei@users.noreply.github.com>
2023-05-31 16:35:18 +0300

    Home Work 2

--- Seminar2/Example001/Program.cs
+++ Seminar2/Example001/Program.cs
@@ -1,1 +1,67 @@
-﻿
+﻿/*Random rnd = new Random();
+int rndNum = rnd.Next(10, 100); // 10- 99
+
+int a = rndNum % 10;
+int b = rndNum / 10;
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{ 
+    Console.WriteLine(b);
+}
+
+
+
+void GenNumbers(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(10, 100);
+    }
+}
+int[] arr = new int[10];
+GenNumbers(arr);
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+    int a = arr[i] % 10;
+    int b = arr[i] / 10;
+    if (a > b)
+    {
+        Console.WriteLine(a);
+    }
+    else
+    {
+        Console.WriteLine(b);
+    }
+}
+*/
+
+/*
+Random rnd = new Random();
+int rndNum = rnd.Next(100, 1000);
+Console.WriteLine(rndNum);
+int a = rndNum / 100;
+int c = rndNum % 10;
+int result = 10 * a + c;
+Console.Write(result);
+*/
+
+
+Random rnd = new Random();
+int rndNum = rnd.Next(1, 10);
+Console.WriteLine(rndNum);
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (b % a == 0)
+{
+    Console.WriteLine("Multiple");
+}
+else
+{
+    Console.WriteLine(b % a);
+}
+
