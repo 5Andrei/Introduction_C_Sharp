e181903452f233a13edb2788d87025057fd66242
5Andrei <120333207+5Andrei@users.noreply.github.com>
2023-06-02 12:18:55 +0300

    Foilders2

---
+++ Lesson2/Example010_methodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 2, 55, 44, 66, 8787, 444, 56, };
+
+int n = array.Length;
+int find = 66;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);          
+    }
+    index++;
+
+}
