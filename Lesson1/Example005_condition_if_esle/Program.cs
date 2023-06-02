2e856720fc2a259c7764e3d17c534169e806514f
5Andrei <120333207+5Andrei@users.noreply.github.com>
2023-06-02 12:18:24 +0300

    Foilders

---
+++ Lesson1/Example005_condition_if_esle/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура! это же МАРИЯ!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
---
+++ Lesson1/Example007_Cyсle/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 20,
+    xc = 80, yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("=");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("=");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("=");
+
+int z = xa, t = xb;
+int count = 0;
+
+while (count < 10000)
+{
+    int what = new Random().Next(0, 3);
+    if (what == 0)
+    {
+        z = (z + xa) / 2;
+        t = (t + ya) / 2;
+    }
+    if (what == 1)
+    {
+        z = (z + xb) / 2;
+        t = (t + yb) / 2;
+    }
+    if (what == 2)
+    {
+        z = (z + xc) / 2;
+        t = (t + yc) / 2;        
+    }
+    Console.SetCursorPosition(z, t);
+    Console.WriteLine("=");
+    count++;
+}
