903307e62fee64601e204f5e18c91a17874a0198
5Andrei <120333207+5Andrei@users.noreply.github.com>
2023-06-04 20:49:27 +0300

    Home work 2

--- Seminar2/HomeWork02/Program.cs
+++ Seminar2/HomeWork02/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Console.Write("enter a three-digit number: ");
+int Tdn = int.Parse(Console.ReadLine());
+int thirdDigit = Tdn / 10 % 10;
+Console.Write(thirdDigit);
+*/
+/*
+int number = ReadInt("Enter digits: ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("No third digit : ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+Console.Write("enter a digit number from 1 to 7: ");
+int DayNum = int.Parse(Console.ReadLine());
+if (DayNum == 6 || DayNum == 7)
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write("No");
+}
+*/
