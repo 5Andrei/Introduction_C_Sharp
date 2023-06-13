321532af36c3b1d19349369651ae8c104050275b
5Andrei <120333207+5Andrei@users.noreply.github.com>
2023-06-13 13:14:50 +0300

    Home Work 5

---
+++ Seminar_5/HomeWork_5/Program.cs
@@ -0,0 +1,80 @@
+﻿/*
+int[] GetArray(int size, int minValue, int maxValue)
+
+{
+    int[] resultArray = new int[size];
+
+    for (int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
+int[] array = GetArray(4, 100, 999);
+
+int Even(int[] arr)
+{
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] % 2 == 0)
+count += 1;
+}
+return count;
+}
+Console.WriteLine($"Массив: [ {String.Join("; ", array)}]");
+Console.WriteLine($"Количество четных чисел: {Even(array)}");
+*/
+/*
+int[] GetArray(int size, int minValue, int maxValue)
+
+{
+    int[] resultArray = new int[size];
+
+    for (int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
+int[] array = GetArray(4, -100, 100);
+
+int Summ(int[] arr)
+{
+int sumOdd = 0;
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] % 2 != 0)
+sumOdd += array[i];
+}
+return sumOdd;
+}
+Console.WriteLine($"Массив: [ {String.Join("; ", array)}]");
+Console.WriteLine($"Summ of odd numbers is: {Summ(array)}");
+*/
+
+
+/*
+int size = 8;
+double[] array = new double[size];
+double max = 0;
+double min = 100;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Math.Round((new Random().NextDouble() * new Random().Next(10, 101)), 2);
+}
+for (int j = 0; j < array.Length; j++)
+{
+
+    if (array[j] > max)
+        max = array[j];
+    if (array[j] < min)
+        min = array[j];
+
+}
+
+double diff = Math.Round((max - min), 2);
+Console.WriteLine($"Массив вещественных чисел: [ {String.Join("; ", array)}],");
+Console.WriteLine($"Разница: {max},- {min}, = {diff}");
+*/
--- Seminar_5/Methods/Program.cs
+++ Seminar_5/Methods/Program.cs
@@ -18,7 +18,7 @@
 // тип_возр_значения ИмяМетода (параметы)
 // PascalCase
 // calculate - переменная, Calculate - имя метода
-int Calculate(int a, int b, char sign)
+/*int Calculate(int a, int b, char sign)
 // a - число 1, b - число; char - 1 символ - '+'
 {
     int result = 0;
@@ -40,3 +40,123 @@ int Calculate(int a, int b, char sign)
 Console.WriteLine(Calculate(100,25,'-')); // 100 - 25 
 Console.WriteLine(Calculate(100,400,'+')); // 100 + 400 
 Console.WriteLine(Calculate(100,400,'*')); // 100 * 400  -> 0
+*/
+/*
+int[] GetArray(int size, int minValue, int maxValue)
+// size = 12, minValue = -9, maxValue = 9
+{
+    int[] resultArray = new int[size];
+
+    for (int i = 0; i < resultArray.Length; i++) // i < resultArray.Length = i < size
+    {
+        resultArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
+
+int[] array = GetArray(12, -9, 9);
+// Интерполяция ($)
+Console.WriteLine($"Массив: [ {String.Join("; ", array)} ]");
+
+int positiveSum = 0; // +
+int negativeSum = 0; // -
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        positiveSum += array[i]; // positiveSum = positiveSum + array[i]
+    }
+    else // <=0
+    {
+        negativeSum += array[i]; // negativeSum = negativeSum + array[i] 
+    }
+}
+Console.WriteLine($"Сумма пол: {positiveSum}, отрц.: {negativeSum}");
+*/
+
+/*
+//Тернарный Оператор
+int[] GetArray(int size, int minValue, int maxValue)
+// size = 12, minValue = -9, maxValue = 9
+{
+    int[] resultArray = new int[size];
+
+    for (int i = 0; i < resultArray.Length; i++) // i < resultArray.Length = i < size
+    {
+        resultArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
+
+int[] array = GetArray(12, -9, 9);
+// Интерполяция ($)
+Console.WriteLine($"Массив: [ {String.Join("; ", array)} ]");
+
+int positiveSum = 0; // +
+int negativeSum = 0; // -
+
+for (int i = 0; i < array.Length; i++)
+{
+    // Тернарный оператор
+    // условие ? значение_ист : значение_ложное
+    positiveSum += array[i] > 0 ? array[i] : 0; // Если число поло-е, то прибавляю array[i] 
+    // иначе - прибавляю 0
+    negativeSum += array[i] < 0 ? array[i] : 0;
+}
+Console.WriteLine($"Сумма пол: {positiveSum}, отрц.: {negativeSum}");
+*/
+/*
+int[] GetArray(int size, int minValue, int maxValue)
+// size = 12, minValue = -9, maxValue = 9
+{
+    int[] resultArray = new int[size];
+
+    for (int i = 0; i < resultArray.Length; i++) // i < resultArray.Length = i < size
+    {
+        resultArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
+int[] ReverseArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= (-1); //  arr[i] =  arr[i] * (-1)
+    }
+    return arr;
+}
+
+int[] array = GetArray(12, -9, 9);
+// Интерполяция ($)
+Console.WriteLine($"Массив: [ {String.Join("; ", array)} ]");
+Console.WriteLine($"Перевернутый массив: [ {String.Join("; ", ReverseArray(array))} ]");
+*/
+/*
+Console.WriteLine($"Массив: [ {String.Join("; ", array)} ]");
+
+bool FindElement(int[] arr, int number)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == number)
+        {
+            return true; // true - элемент найден
+        }
+    }
+    return false;// false - элемент НЕ найден
+}
+
+int numberForSearch = new Random().Next(11); // [0-10]
+Console.WriteLine($"Рандомное число для поиска: {numberForSearch}");
+if (FindElement(array, numberForSearch)) // FindElement(array,numberForSearch) == true
+{
+    Console.WriteLine($"Число {numberForSearch} в массиве присутствует");
+}
+else // FindElement(array,numberForSearch) == false
+{
+    // $"Текст {переменная} текчст {переменная}"
+    Console.WriteLine($"Число {numberForSearch} в массиве ОТСУТСТВУЕТ");
+}
+*/
+
