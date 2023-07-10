208e5bf4f65236cad898b004d7df10667758f3ac
5Andrei <120333207+5Andrei@users.noreply.github.com>
2023-07-10 13:18:55 +0300

    Creating Home Work 9

---
+++ Seminar_9/HomeWork_9/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+Console.Write("Введите число N: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+/// <summary>
+/// Печать чисел от 1 до N
+/// </summary>
+/// <param name="start"> левая граница (1)</param>
+/// <param name="end"> правая граница (N)</param>
+/// <returns>строчку с числами от 1 до N</returns>
+string GetStringOfNumbers(int start, int end)
+{
+
+    if (start == end) return start.ToString();
+
+
+    return start + ", " + GetStringOfNumbers(start - 1, end);
+}
+Console.WriteLine($" '{GetStringOfNumbers(N, 1)}' ");
+*/
+/*
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+/// <summary>
+/// Печать чисел от 1 до N
+/// </summary>
+/// <param name="start"> левая граница (1)</param>
+/// <param name="end"> правая граница (N)</param>
+/// <returns>строчку с числами от 1 до N</returns>
+string GetStringOfNumbers(int start, int end, int sum)
+{
+
+    if (start > end)
+    {
+        Console.WriteLine($"Sum of real numbers from M to N: {sum}");
+        return start.ToString(); 
+    } 
+    sum = sum +(start++);
+    return GetStringOfNumbers(start, end, sum);
+}
+GetStringOfNumbers(M, N, 0);
+*/
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+/*
+int m = InputNumbers("Enter m: ");
+int n = InputNumbers("Enter n: ");
+
+int functionAkkerman = FunctionOfAkkerman(m, n);
+
+Console.Write($"Function of Akkerman is= {functionAkkerman} ");
+
+int FunctionOfAkkerman(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return FunctionOfAkkerman(m - 1, 1);
+  else return FunctionOfAkkerman(m - 1, FunctionOfAkkerman(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+*/
---
+++ Seminar_9/Presrntation_9/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+int Factorial(int value)
+{
+// Базовый случай - случай выхода из рекурсии, бесконечный вызов
+if (value == 1) return 1;
+// Рекурсивный случай - задание рекурсии
+return value * Factorial(value - 1);
+}
+int N = 4;
+Console.WriteLine($"Факториал от числа {N} = {Factorial(N)}");
+*/
+
+// Задача 63: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Write("Введите число N: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+/// <summary>
+/// Печать чисел от 1 до N
+/// </summary>
+/// <param name="start"> левая граница (1)</param>
+/// <param name="end"> правая граница (N)</param>
+/// <returns>строчку с числами от 1 до N</returns>
+string GetStringOfNumbers(int start, int end)
+{
+    // Базовый случай - выход
+    if (start == end) return start.ToString(); // Вернул последнее число N в формате строчки
+
+    // Рекурсивный случай
+    return start + ", " + GetStringOfNumbers(start + 1, end);
+}
+Console.WriteLine($" '{GetStringOfNumbers(1, N)}' "); // start = 1, end = N
+
+
+/*
+// Задача 67: Напишите программу,
+// которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+Console.Write("Введите число N: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int GetDigitsSum(int number)
+{
+if (number == 0) return 0;
+
+return number % 10 + GetDigitsSum(number / 10);
+}
+
+
+Console.WriteLine($"Сумма цифр в числе: {N} = {GetDigitsSum(N)}" );
+*/
